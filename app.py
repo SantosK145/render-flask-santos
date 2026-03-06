0a06b25a0bb0b33b1182e29390baefc6108541e4
SantosK145 <Jesus2020.jsasa@gmail.com>
2026-03-06 10:29:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "API funcionando en Render"
+    return "API funcionando en Render para probar el despliegue de la aplicación Flask"
 
 
 if __name__ == "__main__":
